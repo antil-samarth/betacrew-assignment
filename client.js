9cc0c436fdbc387551f3512ac20f4deba10f92ea
Samarth <antil.samarth@gmail.com>
2024-08-06 20:48:41 +0530

    Added functionality to parse incoming data and save it to json file

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const fs = require('fs');
 
 const HOST = 'localhost';
 const PORT = 3000;
@@ -21,8 +22,32 @@ function sendRequest() {
 client.on('data', (data) => {
     console.log('Received response!');
     console.log('Data : ', data);
+    console.log('Data length : ', data.length);
+    parseData(data);
     responseReceived = true;
 });
+
+function parseData(data) {
+    numPackets = data.length / 17;
+
+    let packets = [];
+
+    for (let i = 0; i < numPackets; i++) {
+        const packet = {
+            symbol: data.toString('ascii', i * 17, i * 17 + 4).trim(),
+            buysellindicator: data.toString('ascii', i * 17 + 4, i * 17 + 5).trim(),
+            quantity: data.readInt32BE(i * 17 + 5),
+            price: data.readInt32BE(i * 17 + 9),
+            packetSequence: data.readInt32BE(i * 17 + 13),
+        }
+        console.log(packet);
+
+        packets.push(packet);
+    }
+
+    fs.writeFileSync('response.json', JSON.stringify(packets, null, 2));
+    console.log('Saved response to response.json');
+}
 
 client.on('close', () => {
     console.log('Connection closed');
