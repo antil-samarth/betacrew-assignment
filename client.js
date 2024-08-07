e406fd610192205ae5ac7813d9e95dbad2bce965
Samarth <antil.samarth@gmail.com>
2024-08-07 11:17:49 +0530

    Added comments

--- client.js
+++ client.js
@@ -4,24 +4,28 @@ const fs = require('fs');
 const HOST = 'localhost';
 const PORT = 3000;
 
+// Create a new socket
 const client = new net.Socket();
 let ifResponseReceived = false;
 
 const receivedPackets = [];
 const receivedSequence = [];
 
+// Connect to server
 client.connect(PORT, HOST, () => {
     console.log('Connected to server');
     sendRequest();
 });
 
+// Function to send request to stream all packets
 function sendRequest() {
     const buffer = Buffer.alloc(2);
-    buffer.writeUInt8(1, 0);
+    buffer.writeUInt8(1, 0); // Request type - Stream All Packets
     console.log('Sending request to stream all packets');
     client.write(buffer);
 }
 
+// Event handler for data received from server
 client.on('data', (data) => {
     console.log('Received response!');
     //console.log('Data : ', data);
@@ -34,8 +38,9 @@ client.on('data', (data) => {
     ifResponseReceived = true;
 });
 
+// Function to parse received data to packets
 function parseData(data) {
-    const numPackets = data.length / 17;
+    const numPackets = data.length / 17; // 17 bytes per packet as defined in Response Payload Format (4 + 1 + 4 + 4 + 4)
     console.log('Number of packets:', numPackets);
     const packets = [];
 
@@ -52,8 +57,9 @@ function parseData(data) {
     return packets;
 }
 
+// Function to save received packets to file
 function saveToFile(packets) {
-    packets.sort((a, b) => a.packetSequence - b.packetSequence);
+    packets.sort((a, b) => a.packetSequence - b.packetSequence); // Sort packets by sequence number
     try {
         fs.writeFileSync('response.json', JSON.stringify(packets, null, 2));
         console.log('Saved response to response.json');
@@ -62,28 +68,30 @@ function saveToFile(packets) {
     }
 }
 
+// Function to find missing packets using sequence number
 function missingSequenceFind() {
     const expectedSequence = Array.from({ length: receivedSequence[receivedSequence.length - 1] + 1 }, (_, i) => i);
-    let a = expectedSequence.filter(num => !receivedSequence.includes(num));
-    a.shift();
-    return a;
+    let missingSeq = expectedSequence.filter(num => !receivedSequence.includes(num));
+    missingSeq.shift(); // SInce packets start from 1 and not 0
+    return missingSeq;
 }
 
+// Function to request missing packets
 function getMissingPackets(missingSequence) {
     client.connect(PORT, HOST, () => {
         console.log('Reconnected to server');
         missingSequence.forEach(num => {
             const buffer = Buffer.alloc(2);
-            buffer.writeUInt8(2, 0);
-            buffer.writeUInt8(num, 1);
+            buffer.writeUInt8(2, 0); // Write request type Resend Packet
+            buffer.writeUInt8(num, 1); // Write missing sequence number
             console.log(`Requesting packet sequence number ${num}`);
             client.write(buffer);
         })
         client.end();
     });
-
 }
 
+// Event handler for connection close
 client.on('close', () => {
     console.log('Connection closed');
     if (!ifResponseReceived) {
@@ -104,7 +112,7 @@ client.on('error', (err) => {
     return
 });
 
-setTimeout(() => {
+setTimeout(() => { // Set timeout for connection
     if (!ifResponseReceived) {
         console.log('tiemout');
         client.destroy();
